877b0e7c143cb372d2697d789e9f511bdc9a38c1
Marcos Vinicius Da Silva <134564570+MarcssVinicius@users.noreply.github.com>
2023-07-26 18:54:56 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroEstaAtito = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtito) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
